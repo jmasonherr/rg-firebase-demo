61344c4ad5ced01753a22b29930f15f59a8b6eab
Jeremy Mason-Herr <jmasonherr@gmail.com>
2021-05-21 12:10:51 -0700

    added firestore element

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
 import React from 'react';
+import firebase from "firebase";
 import logo from './logo.svg';
 import './App.css';
+
+import { useCollection } from 'react-firebase-hooks/firestore';
+
+const FirestoreCollection = () => {
+  const [value, loading, error] = useCollection(
+      firebase.firestore().collection('demo'),
+      {
+        snapshotListenOptions: { includeMetadataChanges: true },
+      }
+  );
+  return (
+      <div>
+        <p>
+          {error && <strong>Error: {JSON.stringify(error)}</strong>}
+          {loading && <span>Collection: Loading...</span>}
+          {value && (
+              <span>
+            Collection:{' '}
+                {value.docs.map((doc) => (
+                    <React.Fragment key={doc.id}>
+                      {JSON.stringify(doc.data())},{' '}
+                    </React.Fragment>
+                ))}
+          </span>
+          )}
+        </p>
+      </div>
+  );
+};
 
 function App() {
   return (
@@ -19,6 +49,7 @@ function App() {
           Learn React
         </a>
       </header>
+        <FirestoreCollection/>
     </div>
   );
 }
